b21fc3343a51d6f4354f8ff3722c0f26c27bf68d
Michal <michalzak0@gmail.com>
2019-02-28 02:40:20 +0100

    Put method done, a little bit of refactoring and delete User class permanently, message class is all we need.

--- app/src/main/java/com/studia/michalzak/newwebservice/Message.java
+++ app/src/main/java/com/studia/michalzak/newwebservice/Message.java
@@ -3,14 +3,13 @@ package com.studia.michalzak.newwebservice;
 import android.util.JsonReader;
 import android.util.Log;
 
+import com.google.gson.Gson;
+
 public class Message {
     String title;
     String body;
-
-    public Message(String title, String body) {
-        this.title = title;
-        this.body = body;
-    }
+    String id;
+    String userId;
 
     public Message(JsonReader jsonReader){
         try {
@@ -19,9 +18,18 @@ public class Message {
                 String key = jsonReader.nextName();
                 if (key.equals("title")) {
                     title = jsonReader.nextString();
-                } else if (key.equals("body")) {
+                }
+                else if (key.equals("body")) {
                     body = jsonReader.nextString();
-                } else {
+                }
+                else if (key.equals("userId")) {
+                    userId = jsonReader.nextString();
+
+                }
+                else if (key.equals("id")) {
+                    id = jsonReader.nextString();
+                }
+                else {
                     jsonReader.skipValue();
                 }
             }
@@ -31,8 +39,21 @@ public class Message {
         }
     }
 
+    public Message(String title, String body, String id, String userId) {
+        this.title = title;
+        this.body = body;
+        this.id = id;
+        this.userId = userId;
+    }
+
     @Override
     public String toString(){
-        return "Title: " + title + "\n" + "Message: " + body + "\n";
+        return "UserID: " + userId + ", messageId: " + id + "\n" + "Title: " + title + "\n" + "Message: " + body + "\n";
+    }
+
+    public String toJson(){
+        Gson gson = new Gson();
+        String json = gson.toJson(this);
+        return json;
     }
 }
--- app/src/main/java/com/studia/michalzak/newwebservice/PutActivity.java
+++ app/src/main/java/com/studia/michalzak/newwebservice/PutActivity.java
@@ -1,13 +1,89 @@
 package com.studia.michalzak.newwebservice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+
+import org.json.JSONObject;
+
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class PutActivity extends AppCompatActivity {
+
+    private EditText editUserId;
+    private EditText editTextTitle;
+    private EditText editTextBody;
+    private EditText editTextId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_put);
+
+        editUserId = findViewById(R.id.editTextIdPut);
+        editTextTitle = findViewById(R.id.editTextTitlePut);
+        editTextBody = findViewById(R.id.editTextBodyPut);
+        editTextId = findViewById(R.id.editTextPostId);
+    }
+
+    public void onClickPut(View v) {
+        if(fieldsEmpty()) {
+            Toast.makeText(PutActivity.this, "Pola nie mogą być puste!", Toast.LENGTH_SHORT).show();
+        } else {
+            makeRequest();
+        }
+    }
+
+    private void makeRequest() {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(MainActivity.apiEndpoint + "/" + editTextId.getText().toString());
+                    HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+                    httpsURLConnection.setRequestMethod("PUT");
+
+                    Message message = getMessageFromBoxes();
+
+                    JSONObject postData = new JSONObject(message.toJson());
+                    httpsURLConnection.setDoOutput(true);
+                    httpsURLConnection.getOutputStream().write(postData.toString().getBytes());
+                    if(httpsURLConnection.getResponseCode() == 200) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(PutActivity.this, "Post zakończony sukcesem!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        httpsURLConnection.disconnect();
+                    }
+                } catch (Exception exception) {
+                    Log.e("Coś poszło nie tak!: ", exception.getMessage());
+                }
+            }
+        });
+    }
+
+    private Message getMessageFromBoxes(){
+        String userId = editUserId.getText().toString();
+        String title = editTextTitle.getText().toString();
+        String body = editTextBody.getText().toString();
+        String id = editTextId.getText().toString();
+        return new Message(title, body, id, userId);
+    }
+
+    private boolean fieldsEmpty(){
+        return editUserId.getText().toString().equals("")
+                || editTextTitle.getText().toString().equals("")
+                || editTextBody.getText().toString().equals("")
+                || editTextId.getText().toString().equals("");
     }
 }
